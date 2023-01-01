d170b912ce500da2f4675917729348356c1bd9b9
FirePrincess <sarahsteinbrecher01@gmx.at>
2023-01-01 17:33:07 +0100

    initial commit

---
+++ scripts/vcpkg_install.ps1
@@ -0,0 +1,6 @@
+
+
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg\vcpkg.exe install glew --triplet=x64-windows-static
+.\vcpkg\vcpkg.exe install freeglut --triplet=x64-windows-static
+.\vcpkg\vcpkg.exe install SOIL --triplet=x64-windows-static
