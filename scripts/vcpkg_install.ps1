452fe5253a360d715f967153db682e116648aa17
FirePrincess <sarahsteinbrecher01@gmx.at>
2023-04-07 17:36:20 +0200

    copy assests to exe

--- scripts/vcpkg_install.ps1
+++ scripts/vcpkg_install.ps1
@@ -1,6 +1,4 @@
 
 
 .\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg.exe install glew --triplet=x64-windows-static
-.\vcpkg\vcpkg.exe install freeglut --triplet=x64-windows-static
-.\vcpkg\vcpkg.exe install SOIL --triplet=x64-windows-static
+.\vcpkg\vcpkg.exe install 
